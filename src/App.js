7774b5ee951a093867dd4ee264af283df688f18f
celestiahall44 <celestia.hall44@gmail.com>
2026-03-12 08:27:37 -0600

    yes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Books from './pages/Books';
 
@@ -11,8 +11,10 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <Route path="/" exact component={Home} />
-        <Route path="/books" component={Books} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/books" component={Books} />
+        </Switch>
         <Footer />
       </div>
     </Router>
